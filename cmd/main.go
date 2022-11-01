8b84f3afccf64696a3657998c8e3e0871a68e569
vitalis-virtus <vshayuk@gmail.com>
2022-11-02 01:12:53 +0200

    transferred to clean architecture

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,15 @@ import (
 
 	"github.com/vitalis-virtus/todo-app-go"
 	"github.com/vitalis-virtus/todo-app-go/pkg/handler"
+	"github.com/vitalis-virtus/todo-app-go/pkg/repository"
+	"github.com/vitalis-virtus/todo-app-go/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatal("error occuredwhile running http server: ", err.Error())
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,8 +1,18 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/vitalis-virtus/todo-app-go/pkg/service"
+)
 
 type Handler struct {
+	service *service.Service
+}
+
+func NewHandler(service *service.Service) *Handler {
+	return &Handler{
+		service: service,
+	}
 }
 
 func (h *Handler) InitRoutes() *gin.Engine {
